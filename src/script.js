e07ee6cbc82669d47769f28fc3f5e95466f7b7df
santaduda209 <mssantaduda@gmail.com>
2024-09-02 23:10:56 +0200

    injected API for the icon

--- src/script.js
+++ src/script.js
@@ -7,6 +7,12 @@ function updateWeather(response) {
   let windSpeedElemet = document.querySelector("#wind-speed");
   let timeElement = document.querySelector("#time");
   let date = new Date(response.data.time * 1000);
+  let iconElement = document.querySelector("#icon");
+
+  iconElement.innerHTML = `<img
+                src="${response.data.condition.icon_url}"
+                class="weather-app-icon"
+              />`;
 
   timeElement.innerHTML = formatDate(date);
   cityElement.innerHTML = response.data.city;
